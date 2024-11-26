7b2bf3622042088ab38328a28a45a8d8b8c32f21
LarryLi <lts60892578@163.com>
2024-11-26 16:16:28 +0800

    update

--- prepare.sh
+++ prepare.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-sudo apt install -y make gcc libssl-dev bc libelf-dev libcap-dev clang gcc-multilib llvm libncurses5-dev \
-     pkg-config libmnl-dev bison flex graphviz iproute2 libbfd-dev
+# sudo apt install -y make gcc libssl-dev bc libelf-dev libcap-dev clang gcc-multilib llvm libncurses5-dev \
+#      pkg-config libmnl-dev bison flex graphviz iproute2 libbfd-dev
 
+sudo apt install -y libbfd-dev clang gcc-multilib
 
 sudo bpftool btf dump file /sys/kernel/btf/vmlinux format c > vmlinux.h
